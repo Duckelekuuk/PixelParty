22f8562d881ede28db54d3858b266aaf7c52b61f
Duckelekuuk <duckelekuuk@gmail.com>
2019-04-14 13:53:24 +0200

    Removed create game method
    Cuz thom is mean

--- src/main/java/com/pixelparty/api/GameManager.java
+++ src/main/java/com/pixelparty/api/GameManager.java
@@ -1,9 +1,7 @@
 package com.pixelparty.api;
 
 import com.pixelparty.api.Games.AbstractGame;
-import org.bukkit.entity.Player;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 
 public final class GameManager {
@@ -14,18 +12,5 @@ public final class GameManager {
 
     public static GameManager getInstance() {
         return instance;
-    }
-
-    public <T extends AbstractGame> T StartMupliplayerGame(Class<T> game, Player... players) {
-        try {
-            AbstractGame gameInstance = game.getDeclaredConstructor(game).newInstance(players);
-            runningGames.add(gameInstance);
-
-            return game.cast(gameInstance);
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 }
