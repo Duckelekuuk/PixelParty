4a8a7c9af019b85170558f9be2a8fa4ca95553ff
Duckelekuuk <duckelekuuk@gmail.com>
2019-04-14 12:55:55 +0200

    Added abstractInventory

---
+++ src/main/java/com/pixelparty/api/inventory/AbstractInventory.java
@@ -0,0 +1,41 @@
+package com.pixelparty.api.inventory;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.inventory.*;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public abstract class AbstractInventory implements InventoryHolder {
+
+    private Inventory inventory;
+
+    public AbstractInventory() {
+        this.inventory = Bukkit.createInventory(this, 54);
+    }
+
+    public AbstractInventory(int size) {
+        this.inventory = Bukkit.createInventory(this, size);
+    }
+
+    public AbstractInventory(int size, String title) {
+        this.inventory = Bukkit.createInventory(this, size, title);
+    }
+
+    public AbstractInventory(InventoryType inventoryType) {
+        this.inventory = Bukkit.createInventory(this, inventoryType);
+    }
+
+    public AbstractInventory(InventoryType inventoryType, String title) {
+        this.inventory = Bukkit.createInventory(this, inventoryType, title);
+    }
+
+    @Override
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void onDrag(InventoryDragEvent event) {}
+    public void onClick(InventoryClickEvent event) {}
+    public void onOpen(InventoryOpenEvent event) {}
+    public void onClose(InventoryCloseEvent event) {}
+}
