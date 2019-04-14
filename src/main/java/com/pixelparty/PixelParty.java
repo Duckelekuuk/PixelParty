b9912ce6aca33bcd52e2e73653c7e9d2b487dd7c
Thom de Meulder <thomdemeulder@gmail.com>
2019-04-14 11:21:02 +0200

    update main class name
    the name should have been different
    so I fixed that

--- src/main/java/com/pixelparty/PixelParty.java
+++ src/main/java/com/pixelparty/PixelParty.java
@@ -2,7 +2,7 @@ package com.pixelparty;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Pixelparty extends JavaPlugin {
+public final class PixelParty extends JavaPlugin {
 
     @Override
     public void onEnable() {
